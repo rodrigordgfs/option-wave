ef6cbd8785c3a0a68b77f496392327b0fbe6ae4a
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-02-09 17:28:52 -0300

    Feature: Added TailwindCSS

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-zinc-950 w-full h-full min-h-screen">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import path from 'path'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  alias: {
+    "@": path.resolve(__dirname, "./"),
+    "~": path.resolve(__dirname, "./"),
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
